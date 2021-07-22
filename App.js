701bb5617b10a57e12955ff3a340ac1c4512de82
jay75chauhan <chauhan75jayk@gmail.com>
2021-07-22 16:39:58 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,21 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { StyleSheet, Text, View } from "react-native";
+import LoginScreen from "./screens/LoginScreen";
 
 export default function App() {
+  const Stack = createStackNavigator();
+
+  const globalScreenOptions = {
+    headerStyle: { backgroundColor: "#2C6BED" },
+    headerTitleStyle: { color: "white" },
+    headerTintColor: "white",
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={globalScreenOptions}>
+        <Stack.Screen name="Login" component={LoginScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ screens/LoginScreen.js
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from "react-native";
+import * as React from "react";
+import { Button, Input, Image } from "react-native-elements";
+import { StatusBar } from "expo-status-bar";
+
+const LoginScreen = () => {
+  return (
+    <View>
+      <StatusBar style="light" />
+      <Text>Login Screen</Text>
+    </View>
+  );
+};
+
+export default LoginScreen;
+const styles = StyleSheet.create({});
